7b9a034ecf4224726a85ba24b71c6fee3d9c657c
Mark <redddogjr@gmail.com>
2017-08-22 12:43:18 -0600

    Finding one word in one file works pretty well. need to expand to many words and many files

--- ex26_logfind/src/main.c
+++ ex26_logfind/src/main.c
@@ -32,25 +32,65 @@ feature is done.
 #include <stdlib.h>
 #include "dbg.h"
 
+#define MAX_LINE_LEN 80
+
+char line[MAX_LINE_LEN];
+
+void reset_line()
+{
+    int i;
+    for (i = 0; i < MAX_LINE_LEN; i++) {
+        line[i] = '\0';
+    }
+}
+
 int check_file(char* filename, char* word, unsigned int wordlen)
 {
-    FILE* filer = fopen(filename, "r");
-    int out;
-    char ch = ' ';
+    FILE* rfile = fopen(filename, "r");
+    check(rfile, "File not found!");
+    
+    char ch = ' ';  // make sure that ch != EOF
+    
+    int line_number = 1, // initalize line_number to 1
+        li          = 0, // line index
+        wi          = 0, // word index
+        look        = 0, // bool; if look == 1 we have found the first letter
+        found       = 0, // bool; found the word on a line
+        infile      = 0; // bool; returns 1 for at least one instance in file
+    
     while (ch != EOF){
-        ch = fgetc(filer);
-        if (ch == word[0]){
-            int i;
-            for (i = 1; i < wordlen; i++) {
-                ch = fgetc(filer);
-                if (ch != word[i]) break;
-            }
-            if (i == wordlen) out = 1;
-            else out = 0;
+        // get a char
+        ch = fgetc(rfile);
+        // if it fits add the char to the line array
+        if (li < MAX_LINE_LEN && ch != '\n') line[li++] = ch;
+        // if you see the first letter start looking for the rest
+        if (ch == word[0]) look = 1;
+        // check if you have found the next letter in the word
+        if (wi < wordlen && ch == word[wi] && look) wi++;
+        // otherwise you reset the word index and stop looking for the word
+        else {
+            wi = 0;
+            look = 0;
+        }
+        // if you have found a sufficient number of correct letters you have
+        // found the word
+        if (wi == wordlen) found = 1;
+        // print results and reset everything when you find a new line
+        if (ch == '\n') {
+           if (found){
+               infile += 1;
+               printf("Found in file \"%s\" On line %d: ", filename, line_number);
+               printf("\"%s\"\n", line);
+               found = 0;
+           }
+           reset_line();
+           li = 0;
+           wi = 0;
+           line_number ++; 
         }
     }
-    check(fclose(filer) == 0, "File Not Found.");
-    return out;
+    check(fclose(rfile) == 0, "Error with file.");
+    return infile;
     
 error:
     return -1;
@@ -61,10 +101,11 @@ int main (int argc, char* argv[])
 {
     char* ts = argv[1];
     unsigned int tslen = strlen(ts);
-    char* fname = "dat.log";
+    printf("\nstring len %d\n", tslen);
+    char* fname = "data.log";
     int out = check_file(fname, ts, tslen);    
-    printf("test %d\n", tslen);
-    printf("%d\n", out);
+    
+    printf("Found \"%s\" %d times.\n",ts, out);
     check(out != -1, "Error with file check");
     return 0;
 
