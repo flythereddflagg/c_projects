750c5d98ec8f33e2719ceb362c5eef185119b755
Mark Redd <mark.redd@intel.com>
2025-01-24 16:31:02 -0700

    set up the next test

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 /*
+
+
+
+Exercise 3-1. Our binary search makes two tests inside the loop, 
+when one would suffice (at the price of more tests outside.) 
+Write a version with only one test inside the loop and measure 
+the difference in run-time.
+*/
+#include <stdio.h>
+
 // binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] 
 int binsearch(int x, int v[], int n){
     int low, high, mid;
@@ -16,12 +26,22 @@ int binsearch(int x, int v[], int n){
     return -1; // no match 
 }
 
-Exercise 3-1. Our binary search makes two tests inside the loop, 
-when one would suffice (at the price of more tests outside.) 
-Write a version with only one test inside the loop and measure 
-the difference in run-time.
-*/
-#include <stdio.h>
+int better_binsearch(int x, int v[], int n){
+    //edit this one!
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low+high)/2;
+        if (x < v[mid])
+            high = mid + 1;
+        else if (x > v[mid])
+            low = mid + 1;
+        else // found match
+            return mid;
+    }
+    return -1; // no match 
+}
 
 int main(){
 
