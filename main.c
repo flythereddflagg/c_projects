bdf83028be7d713d015c3fd53f938b658f7ac950
Mark Redd <mark.redd@intel.com>
2025-02-04 14:46:50 -0700

    added more to leeson but still needs work

--- main.c
+++ main.c
@@ -29,15 +29,39 @@ void escape(char* src, char* dest){
 }
 
 void un_escape(char* src, char* dest){
-
+    int offset = 0, i = 0, esc = 0;
+    for (i = 0; src[i] != '\0'; i++){
+        if ('\\' != src[i] || esc){
+            dest[i+offset] = src[i];
+            esc = 0;
+            continue;
+        }
+        esc = 1;
+        switch (src[i+1]){
+            case 'n':
+                dest[i+offset] = '\n';
+                break;
+            case 't':
+                dest[i+offset] = '\t';
+                break;
+            default:
+                dest[i+offset] = src[i];
+                break;
+        }
+        offset --;
+    }
+    dest[i+offset] = '\0';
 }
 
 
 int main(){
     char in[30] = "the\n quick brown\t fox\n!";
     char out[30];
+    char out2[30];
     printf("'%s'\n", out);
     escape(&(in[0]), &(out[0]));
     printf("escape--> '%s'\n", out);
+    un_escape(&(out[0]), &(out2[0]));
+    printf("unescape--> '%s'\n", out2);
     return 0;
 }
