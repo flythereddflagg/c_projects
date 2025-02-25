90137199499486da94e810235a9c1c4b664ef928
Mark Redd <mark.redd@intel.com>
2025-02-25 14:44:29 -0700

    did 3-4. two more exercises on this are availible.

--- main.c
+++ main.c
@@ -17,6 +17,39 @@ padded with blanks on the left if necessary to make it wide enough.
 #include "dbg.h"
 
 
+void reverse(char line[], int len){
+    int i = 0;
+    char tmp = 0;
+    for (i = 0; i < (len/2); i++){
+        tmp = line[len-i-1];
+        line[len-i-1] = line[i];
+        line[i] = tmp;
+    }
+}
+/* itoa: convert n to characters in s */
+void itoa(int n, char s[]){
+    int i, sign;
+    unsigned int n2;
+    if ((sign = n) < 0) /* record sign */
+        n2 = (unsigned int) -n; /* make n positive */
+    else
+        n2 = (unsigned int) n;
+    i = 0;
+    do { /* generate digits in reverse order */
+        s[i++] = n2 % 10 + '0'; /* get next digit */
+    } while ((n2 /= 10) > 0); /* delete it */
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s, i);
+}
+
+
+
 int main(){
+    int t1 = 0b10000000000000000000000000000000;
+    char s[90] = "";
+    itoa(t1, s);
+    printf("%d : '%s'\n", t1, s);
     return 0;
 }
