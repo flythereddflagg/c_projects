6816afff5aa1b145af92df58f560f5d2292df89d
Mark Redd <mark.redd@intel.com>
2026-01-28 17:10:45 -0700

    make the A go in circles

--- main.c
+++ main.c
@@ -1,18 +1,32 @@
 #include "raylib.h"
+#include "raymath.h"
 #include "dbg.h"
 
 
 int main(void)
 {
-    InitWindow(640, 360, "raylib [core] example - basic window");
+    Vector2 pos = {100, 100}, newpos = {1, 1};
+    int i = 1;
+    InitWindow(640, 360, "Test Game");
+    SetTargetFPS(15);
 
     while (!WindowShouldClose())
     {
-        
+        i += 1;
+        newpos.x = cos(i/100.0);
+        newpos.y = sin(i/100.0);
         BeginDrawing();
         {
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 100, 100, 20, LIGHTGRAY);
+            DrawTextEx(
+                GetFontDefault(),
+                "A",
+                Vector2Add(Vector2Multiply(pos, newpos), pos),
+                20,
+                0, 
+                BLACK
+            );
+            printf("%f, %f\n", Vector2Multiply(pos, newpos).x, Vector2Multiply(pos, newpos).y);
         }
         EndDrawing();
     }
